62fac54f8f40985b5655d7780a21795fa4a5e4f1
Sergio Fonseca <sergio.fonseca2712@hotmail.com>
2020-06-11 18:34:45 -0600

    build proyect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ import { AuthGuard } from './auth.guard';
 const routes: Routes = [
   {
     path : '',
-    redirectTo :'/home',
+    component: CatalogueComponent,
     pathMatch : 'full'
   },
   {
